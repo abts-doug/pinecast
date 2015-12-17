15d33d2818e3e4f578efdf25dce7a9bc32ad68f7
Matt Basta <me@mattbasta.com>
2015-12-16 23:10:00 -0800

    Refactor importer_worker to use boto3 for sns

--- dashboard/importer_worker.py
+++ dashboard/importer_worker.py
@@ -1,17 +1,21 @@
 import json
 
 import boto.sns
+from boto3.session import Session
 from django.conf import settings
 
 
 def push_batch(bus, payloads):
-    conn = boto.sns.connect_to_region(
-        'us-east-1', # TODO: make this an env variable?
-        aws_access_key_id=settings.SQS_ACCESS_ID,
-        aws_secret_access_key=settings.SQS_SECRET_KEY)
+    session = Session(aws_access_key_id=settings.SQS_ACCESS_ID,
+                      aws_secret_access_key=settings.SQS_SECRET_KEY,
+                      region_name='us-east-1')  # TODO: make this an env variable?
+    sns = session.client('sns')
     count = 0
     for p in payloads:
-        conn.publish(bus, json.dumps(p))
+        sns.publish(
+            TopicArn=bus,
+            Message=json.dumps(p)
+        )
         count += 1
 
     print 'Dispatched %d events to SNS' % count
